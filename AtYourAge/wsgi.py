b3f19e0741f701fbf529941a28a300e37d886d1f
Alex Freas <alex@taximagic.com>
2013-02-18 12:53:33 -0500

    Updated wsgi script.

--- AtYourAge/wsgi.py
+++ AtYourAge/wsgi.py
@@ -1,7 +1,7 @@
-import os, sys
-sys.path.append('/home/ec2-user/apps/AtYourAge/atyourage-django')
-import site
-site.addsitedir('/home/ec2-user/apps/AtYourAge/lib/python2.7/site-packages')
-os.environ['DJANGO_SETTINGS_MODULE'] = 'atyourage.settings'
+import os
+import sys
+PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
+sys.path.append(PROJECT_PATH)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'EventApp.settings'
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
