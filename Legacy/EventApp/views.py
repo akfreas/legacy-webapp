ced3f01155fa00c55abfd6154c2211345880d652
Alex Freas <akfreas@gmail.com>
2013-10-01 17:16:52 -0400

    Not recording date_last_seen on every get of user, instead only on relevant gets.

--- Legacy/EventApp/views.py
+++ Legacy/EventApp/views.py
@@ -37,6 +37,8 @@ def update_birthday(request, user_id):
 
     access_token, requesting_user_id = info_from_request_cookie(request)
     requesting_user = get_or_create_user(requesting_user_id, access_token)
+    requesting_user.date_last_seen = datetime.now()
+    requesting_user.save()
 
     user = get_or_create_user(facebook_id, access_token)
 
@@ -55,6 +57,7 @@ def add_users(request):
 
     access_token, user_id = info_from_request_cookie(request)
     requesting_user = get_or_create_user(user_id, access_token)
+    requesting_user.date_last_seen = datetime.now()
 
 
     for fb_id in person_array:
@@ -72,6 +75,7 @@ def delete_user(request, user):
     access_token, user_id = info_from_request_cookie(request)
     print access_token, user_id
     requesting_user = get_or_create_user(user_id, access_token)
+    requesting_user.date_last_seen = datetime.now()
 
 
     user_to_delete = EventUser.objects.get(facebook_id=user)
@@ -128,14 +132,13 @@ def get_or_create_user(facebook_id, access_token):
 
     try:
         user = EventUser.objects.get(facebook_id=facebook_id)
-        user.date_last_seen = datetime.now()
 
     except EventUser.DoesNotExist:
         user = EventUser(facebook_id=facebook_id)
         user.date_added = datetime.now()
         utils.populate_user_with_fb_fields(user, access_token)
+        user.save()
 
-    user.save()
     return user
 
 def events_for_figure(request, figure_id):
@@ -188,6 +191,7 @@ def events(request):
         return events_for_no_user(request)
 
     requesting_user = get_or_create_user(user_id, access_token)
+    requesting_user.date_last_seen = datetime.now()
     user_friends = EventUser.added_by.through.objects.filter(to_eventuser=requesting_user) 
 
     all_users = user_friends
